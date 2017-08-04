f6b162125fcd81c3228a4e1fdf40d422b498f9ea
rounakpolley <30698500+rounakpolley@users.noreply.github.com>
2017-08-04 21:44:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,13 @@ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
-  res.send("ARTICLE 2");
+res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+    
 });
 
 app.get('/article-three', function (req, res) {
-  res.send("ARTICLE 3");
+res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+    
 });
 
 app.get('/ui/style.css', function (req, res) {

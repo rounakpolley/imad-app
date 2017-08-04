97dc897088458bc7341f397f597bb216928b0240
rounakpolley <30698500+rounakpolley@users.noreply.github.com>
2017-08-04 23:02:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,6 +44,8 @@ function createTemplate(data)
         
     </html>
     `;
+    
+    return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
@@ -51,7 +53,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {

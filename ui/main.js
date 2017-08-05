00f91b5f0473f0fba871ed248ddabd8080b6c6a7
rounakpolley <30698500+rounakpolley@users.noreply.github.com>
2017-08-05 16:33:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 // move img gradually
 var img = document.querySelector('#logo');
-function moveRight()
+function moveRight ()
 {
  marginLeft = marginLeft + 10;
  img.style.marginLeft = marginLeft + 'px';

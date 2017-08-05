053d73c39f33993acfcd0fbb5622974ae9d74850
rounakpolley <30698500+rounakpolley@users.noreply.github.com>
2017-08-05 17:12:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,31 @@ window.onload = function init ()
 console.log('Loaded!');
 // move img gradually
 var img = document.querySelector('#logo');
-
+var position   = 0; //0 for middle left -1 right 1
 var marginLeft = 0;
 function moveRight ()
 {
-    if((marginLeft > 0) && (marginLeft <  100))    marginLeft = marginLeft + 1;
-    if((marginLeft < 0) && (marginLeft > -100))    marginLeft = marginLeft - 1;
+    if(((marginLeft > 0) && (marginLeft <  100)) && position == 0)
+    {
+        marginLeft = marginLeft + 1;
+        if(marginLeft == 100) position = 1;
+    }
+    if(((marginLeft > 0) && (marginLeft <= 100)) && position == 1)
+    {
+        marginLeft = marginLeft - 1;
+        if(marginLeft == 0) position = -1;
+    }
+    if(((marginLeft <= 0) && (marginLeft > -100)) && position == -1)
+    {
+        marginLeft = marginLeft - 1;
+        if(marginLeft == -100) position = 1;
+    }
+    if(((marginLeft < 0) && (marginLeft > -100)) && position == 1)
+    {
+        marginLeft = marginLeft + 1;
+        if(marginLeft == 0) position = 0;
+    }
+    
     img.style.marginLeft = marginLeft + 'px';
 }
 

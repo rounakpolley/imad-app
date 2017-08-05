0510ff5f5d74f77669200d2d98555ddcba8619b2
rounakpolley <30698500+rounakpolley@users.noreply.github.com>
2017-08-05 16:37:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,18 @@
+window.onload = init ()
+{
 console.log('Loaded!');
 // move img gradually
 var img = document.querySelector('#logo');
+
 var marginLeft = 0;
 function moveRight ()
 {
  marginLeft = marginLeft + 10;
  img.style.marginLeft = marginLeft + 'px';
 }
+
 img.onclick = function ()
 {
     var interval = setInterval(moveRight, 100);
+}    
 }

888fa300a5af6727203b130e6aabb601212a7634
rounakpolley <30698500+rounakpolley@users.noreply.github.com>
2017-08-08 19:57:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config =
 {
     user        : 'rounakpolley19972014',
     database    : 'rounakpolley19972014',
-    host        : 'http://db.imad.hasura-app.io',
+    host        : 'db.imad.hasura-app.io',
     port        : '5432',
     password    : process.env.DB_PASSWORD
 };

a35a90fddfa6af4f295e18add7fe88cd416d90eb
rounakpolley <30698500+rounakpolley@users.noreply.github.com>
2017-08-21 21:04:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var Pool = require('pg').Pool;
 var config =
@@ -15,6 +16,7 @@ var config =
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) 
@@ -106,8 +108,10 @@ app.get('/hash/:input', function (req, res)
     res.send(hashedString);
 });
 
-app.get('/create-user', function (req, res)
+app.post('/create-user', function (req, res)
 {
+    var username = req.body.username;
+    var password = req.body.password;
     var salt = crypto.getRandomBytes(128).toString('hex');
     var dbString = hash(password, salt);
     
@@ -115,7 +119,7 @@ app.get('/create-user', function (req, res)
     function (err, result)
     {
         if(err) {   res.status(500).send(err.toString());   }
-        else    {   res.send(JSON.stringify(result.rows));  }
+        else    {   res.send(JSON.stringify(result.rows+' user succesfully created'+username));  }
     });
    
 });
